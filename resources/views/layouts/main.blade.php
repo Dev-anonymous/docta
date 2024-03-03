5e234138c8792cffa5133f9047aa36e66d759721
TheMaz <patriczum@gmail.com2>
2024-03-03 14:14:11 +0200

    34567890-

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta name="theme-color" content="#02BBFF">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>@yield('title') - {{ config('app.name') }} </title>
     @yield('meta')
     <meta name="csrf-token" content="{{ csrf_token() }}" />
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="theme-color" content="#02BBFF">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Connexion | {{ config('app.name') }}</title>
