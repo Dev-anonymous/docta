34e6e1c3b8f4870cf94b5ff2c2071564d3ff7d5d
TheMaz <patriczum@gmail.com2>
2024-09-26 17:32:31 +0200

    3123123

--- app/Http/Controllers/API/DemandeadhesionAPIController.php
+++ app/Http/Controllers/API/DemandeadhesionAPIController.php
@@ -202,7 +202,7 @@ class DemandeadhesionAPIController extends Controller
         try {
             $link = route('web.index', ['docta' => $profil->code]);
             $link2 = asset('docta-admin.apk');
-            $m['msg'] = "Bienvenu chez Docta, votre profil a été approuvé. Votre lien personnel est $link vous pouvez le partarger à vos patients pour qu'ils entrent directement en contact avec vous.\n\nUtilisez l'application mobile pour vous connecter au compte.\n\nEmail : $data->email\nMot de passe : $pass\n\n<a href='$link2'>Télécharger l'application Docteur</a>\n\n\n Coordialement.";
+            $m['msg'] = "Bienvenu chez Docta, votre profil a été approuvé. Votre lien personnel est $link vous pouvez le partarger à vos patients pour qu'ils entrent directement en contact avec vous.\n\nUtilisez l'application mobile pour vous connecter au compte.\n\nEmail : $data->email\nMot de passe : $pass\n\n<a href='$link2'>Télécharger l'application Docteur</a>\n\n";
 
             $m['subject'] = "Profil Docta approuvé";
             Mail::to($data->email)->send(new AppMail((object)$m));
